29441f875ea3177686997325670cad234b861338
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2016-08-07 10:59:33 -0400

    Major changes to applyconf:
    A control file can now be used to control how subdirectories are
    handled.  This control file is an ini file, with each section being
    named after the relative path being handled.  The root section [.] can
    be used to set up initial values.  During directory walking, sub-items
    inherit the state of their parents and override the state based on the
    control entries.

--- applyconf.py
+++ applyconf.py
@@ -10,7 +10,8 @@ __version__     = "0.0.1"
 import sys
 import os
 import argparse
-import re
+import fnmatch
+import shutil
 
 try:
     import ConfigParser as configparser
@@ -26,91 +27,277 @@ except ImportError:
 from mrbaviirc import template
 
 
-def parseargs():
-    """ Parse the command line arguments. """
-    parser = argparse.ArgumentParser(description="Apply configuration templates.")
+class Control(object):
+    """ Represent a control state. """
+    UNSET = 0
+    COPY = 1
+    FOLLOW = 2
+    IGNORE = 3
 
-    parser.add_argument("-n", dest="dryrun", action="store_true", default=False,
-        help="Dry run.  Log messages but dont touch any files.")
-    parser.add_argument("-f", dest="force", action="store_true", default=False,
-        help="Force-build a file even if not out of date."
-    )
-    parser.add_argument("-s", dest="symlink", action="store_true", default=False,
-        help=("Copy symlink that links to a file instead of following it. "
-              "Symlinks to directories and non-regular files are always copied as symlinks.")
-    )
-    parser.add_argument("-e", dest="ext", default=".tmpl",
-        help="Extension for scanned templates.")
-    parser.add_argument("-i", dest="input", required=True,
-        help="Specify the input file or directory.")
-    parser.add_argument("-o", dest="output", required=True,
-        help="Specify the output file or directory.")
-    parser.add_argument("-d", dest="data", required=True,
-        help="Specify the data file.")
-    parser.add_argument(dest="values", nargs="*",
-        help="Specify name=value pairs of data.")
+    def __init__(self, copy=None):
+        """ Initialize a control state. """
+        self.file_symlink = copy.file_symlink if copy else None
+        self.dir_symlink = copy.dir_symlink if copy else None
+        self.other_symlink = copy.other_symlink if copy else None
+        self.exclude = list(copy.exclude) if copy else None
+        self.extension = copy.extension if copy else None
+        self.other = copy.other if copy else None
+        self.ignore = copy.ignore if copy else None
 
-    return parser.parse_args()
+    def clone(self, update):
+        """ Clone and update the control state. """
+        c = Control(self)
+
+        if not update.file_symlink is None:
+            c.file_symlink = update.file_symlink
+
+        if not update.dir_symlink is None:
+            c.dir_symlink = update.dir_symlink
+
+        if not update.other_symlink is None:
+            c.other_symlink = update.other_symlink
+
+        if not update.exclude is None and len(update.exclude):
+            if update.exclude[0] == None:
+                c.exclude = update.exclude[1:]
+            else:
+                c.exclude.extend(update.exclude)
+
+        if not update.extension is None:
+            c.extension = update.extension
+
+        if not update.other is None:
+            c.other = update.other
+
+        if not update.ignore is None:
+            c.ignore = update.ignore
+
+        return c
+
+    def defaults(self):
+        """ Set the defaults. """
+        self.file_symlink = self.FOLLOW
+        self.dir_symlink = self.COPY
+        self.other_symlink = self.COPY
+        self.exclude = [".*", "*~"]
+        self.extension = ".tmpl"
+        self.other = self.IGNORE
+        self.ignore = False
 
 
-def readdata(fn):
-    """ Read the data file and return the resulting dict. """
-    parser = configparser.SafeConfigParser()
-    parser.read(fn)
+class ProgramData(object):
+    """ Represent the program data. """
 
-    result = {}
-    for section in parser.sections():
-        if not section in result:
-            result[section] = {}
+    def __init__(self):
+        """ Initialize the program data. """
+        self.cmdline = None
+        self.data = None
+        self.controls = None
 
-        for (key, value) in parser.items(section):
-            result[section][key] = value
+    def getcmdline(self):
+        """ Parse the command line arguments. """
+        if not self.cmdline is None:
+            return self.cmdline
 
-    return result
+        parser = argparse.ArgumentParser(description="Apply configuration templates.")
 
+        parser.add_argument("-l", dest="live", action="store_true", default=False,
+            help="Make actual changes instead of a dry run.")
+        parser.add_argument("-f", dest="force", action="store_true", default=False,
+            help="Force-build a file even if not out of date."
+        )
+        parser.add_argument("-i", dest="input", required=True,
+            help="Specify the input file or directory.")
+        parser.add_argument("-o", dest="output", required=True,
+            help="Specify the output directory.")
+        parser.add_argument("-d", dest="data",
+            help="Specify the data file.")
+        parser.add_argument("-c", dest="control",
+            help="Specify the control file.")
+        parser.add_argument(dest="values", nargs="*",
+            help="Specify name=value pairs of data.")
 
-def apply(source, dirname, data, args, _env=[None]):
-    """ Apply a template to some data and save the results. """
-    if not _env[0]:
-        context = {}
-        for value in args.values:
+        self.cmdline = parser.parse_args()
+        return self.cmdline
+
+    def getdata(self):
+        """ Read the data file and return the resulting dict. """
+        if not self.data is None:
+            return self.data
+
+        cmdline = self.getcmdline()
+        result = {}
+
+        # Read from data file
+        if cmdline.data:
+            parser = configparser.SafeConfigParser()
+            parser.read(cmdline.data)
+
+            for section in parser.sections():
+                if not section in result:
+                    result[section] = {}
+
+                for (key, value) in parser.items(section):
+                    result[section][key] = value
+
+        # Update with command line values
+        for value in cmdline.values:
             parts = value.split("=", 1)
             if len(parts) == 1:
-                context[parts[0].strip()] = True
+                result[parts[0].strip()] = True
             else:
-                context[parts[0].strip()] = parts[1].strip()
+                result[parts[0].strip()] = parts[1].strip()
 
-        context["lib"] = template.StdLib()
-        _env[0] = template.Environment(context)
+        self.data = result
+        return self.data
 
-        
+    def getcontrols(self):
+        """ Read the control file. """
+        if not self.controls is None:
+            return self.controls
 
-    env = _env[0]
+        cmdline = self.getcmdline()
+        if cmdline.control is None:
+            self.controls = {}
+            return self.controls
+
+        parser = configparser.SafeConfigParser()
+        parser.read(cmdline.control)
+
+        result = {}
+        for section in parser.sections():
+            c = result.setdefault(section, Control())
+
+            for (key, value) in parser.items(section):
+                key = key.strip()
+                value = value.strip()
+
+                if key in ("file_symlink", "dir_symlink", "other_symlink"):
+                    if value == "copy":
+                        value = Control.COPY
+                    elif value == "follow" and key != "other_symlink":
+                        value = Control.FOLLOW
+                    elif value == "ignore":
+                        value = Control.IGNORE
+                    else:
+                        value = None # TODO: error
+
+                    if key == "file_symlink":
+                        c.file_symlink = value
+                    elif key == "other_symlink":
+                        c.other_symlink = value
+                    else:
+                        c.dir_symlink = value
+
+                elif key == "exclude":
+                    if value[0:1] == "+":
+                        c.exclude = []
+                        value = value[1:]
+                    else:
+                        c.exclude = [None]
+                    
+                    parts = value.split(",")
+                    for part in parts:
+                        part=part.strip()
+                        if part:
+                            c.exclude.append(part)
+
+                elif key == "extension":
+                    c.extension = value
+
+                elif key == "other":
+                    if value == "ignore":
+                        c.other = Control.IGNORE
+                    elif value == "copy":
+                        c.other = Control.COPY
+                    else:
+                        pass # TODO: error
+
+                elif key == "ignore":
+                    c.ignore = (value.lower() in ("yes", "on", "true", "1"))
+
+
+        self.controls = result
+        return self.controls
+
+    def getstate(self, path, state):
+        """ Get a new state based on the current state and relpath """
+
+        controls = self.getcontrols()
+
+        path = path.replace(os.sep, "/")
+
+        if not path in controls:
+            return state
+        else:
+            return state.clone(controls[path])
+
+
+def apply(source, dirname, env, state, progdata):
+    """ Apply a template to some data and save the results. """
+    cmdline = progdata.getcmdline()
 
     tmpl = env.load_file(source)
     rndr = template.StringRenderer()
-    tmpl.render(rndr, data)
+    tmpl.render(rndr, progdata.getdata())
 
     for section in rndr.get_sections():
         if not section.startswith("file:"):
             continue
         target = os.path.join(dirname, section[5:])
 
-        if not (os.path.islink(target) or checktimes(source, target) or args.force):
+        if not (os.path.islink(target) or checktimes(source, target) or cmdline.force):
             log("NOCHG", target, source)
             continue
 
         log("BUILD", target, source)
-        if args.dryrun:
+        if not cmdline.live:
             continue
 
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        elif os.path.exists(target):
+        if os.path.exists(target):
             os.unlink(target) # Unlink so if it is a symlink we don't overwrite target of symlink
     
         with open(target, "wt") as handle:
             handle.write(rndr.get_section(section))
+
+
+def copylink(source, target, state, progdata):
+    """ Copy a symbolic link. """
+    cmdline = progdata.getcmdline()
+
+    link = os.readlink(source)
+    log("SYMLN", target, "{0} --> {1}".format(source, link))
+    if not cmdline.live:
+        return
+
+    if os.path.exists(target) or os.path.islink(target):
+        os.unlink(target)
+    os.symlink(link, target)
+
+
+def copyfile(source, target, state, progdata):
+    """ Copy a file. """
+    cmdline = progdata.getcmdline()
+
+    if os.path.islink(target) or checktimes(source, target) or cmdline.force:
+        log("COPY ", target, source)
+        if not cmdline.live:
+            return
+
+        if os.path.exists(target):
+            os.unlink(target)
+
+        shutil.copy(source, target)
+    else:
+        log("NOCHG", target, source)
+
+
+def handlefile(source, target, env, state, progdata):
+    """ Handle a file action. """
+    if source.endswith(state.extension):
+        apply(source, os.path.dirname(target), env, state, progdata)
+    elif state.other == Control.COPY:
+        copyfile(source, target, state, progdata)
 
 
 def checktimes(source, target):
@@ -132,55 +319,84 @@ def log(status, message, extra=None):
         print("{0}: {1}".format(status, message))
 
 
-def walk(root):
-    """ Walk a path and return all files and symlinks. """
-    for item in sorted(os.listdir(root)):
-        path = os.path.join(root, item)
-        if os.path.isdir(path) and not os.path.islink(path):
-            for i in walk(path):
-                yield i
-        else:
-            yield path
+def process(indir, outdir, relpath, env, state, progdata):
+    """ Process a given directory. """
+    cmdline = progdata.getcmdline()
+
+    if not os.path.isdir(outdir) and cmdline.live:
+        os.makedirs(outdir)
+        # TODO: create outdir based on state information
+        # state can contain perms, ownershipm etc
+
+    listing = os.listdir(indir)
+    for entry in sorted(listing):
+
+        # Set some values for the entry
+        entrypath = os.path.join(relpath, entry)
+        inpath = os.path.join(indir, entry)
+        outpath = os.path.join(outdir, entry)
+        entrystate = progdata.getstate(entrypath, state)
+
+        # Check against excludes
+        if entrystate.ignore:
+            continue
+
+        if any(fnmatch.fnmatchcase(entrypath, exclude) for exclude in state.exclude):
+            continue
+
+        # Handle the entry
+        if os.path.isdir(inpath):
+            if os.path.islink(inpath):
+                if entrystate.dir_symlink == Control.FOLLOW:
+                    process(inpath, outpath, entrypath, env, entrystate, progdata)
+                elif entrystate.dir_symlink == Control.COPY:
+                    copylink(inpath, outpath, entrystate, progdata)
+                else:
+                    pass # Other option is ignore
+            else:
+                process(inpath, outpath, entrypath, env, entrystate, progdata)
+
+        elif os.path.isfile(inpath):
+            if os.path.islink(inpath):
+                if entrystate.file_symlink == Control.FOLLOW:
+                    handlefile(inpath, outpath, env, entrystate, progdata)
+                elif entrystate.file_symlink == Control.COPY:
+                    copylink(inpath, outpath, entrystate, progdata)
+                else:
+                    pass # Other option is ignore
+            else:
+                handlefile(inpath, outpath, env, entrystate, progdata)
+
+        elif os.path.islink(inpath): # Not file or directory
+            if entrystate.other_symlink == Control.COPY:
+                copylink(inpath, outpath, entrystate, progdata)
+            else:
+                pass # Other option is ignore
+
 
 
 def main():
     """ Run the program. """
-    args = parseargs()
+    # Basic setup
+    progdata = ProgramData()
+    args = progdata.getcmdline()
 
-    data = readdata(args.data)
+    # Set initial states
+    state = Control()
+    state.defaults()
 
-    # Building just a file
+    # Create our template environment
+    env = template.Environment({"lib": template.StdLib()})
+
+
+    # Build
+    state = progdata.getstate(".", state)
     if os.path.isfile(args.input):
-        apply(args.input, args.output, data, args)
-        sys.exit(0)
+        apply(args.input, args.output, env, state, progdata)
+    else:
+        process(args.input, args.output, "", env, state, progdata)
 
-    # Building a directory
-    for input in walk(args.input):
-        rel = os.path.relpath(input, args.input)
-        output = os.path.join(args.output, rel)
-        dirname = os.path.dirname(output)
-
-        # Walk only returns files and links
-        # If file always build
-        # If link to file, build if not args.symlink
-        # For all other links (such as to directory, or device, etc), copy link
-
-        if os.path.isfile(input) and (not os.path.islink(input) or not args.symlink):
-            if input.endswith(args.ext):
-                apply(input, dirname, data, args)
-        else:
-            link = os.readlink(input)
-            log("SYMLN", output, "{0} --> {1}".format(input, link))
-            if args.dryrun:
-                continue
-
-            if not os.path.isdir(dirname):
-                os.makedirs(dirname)
-            elif os.path.isfile(output) or os.path.islink(output):
-                os.unlink(output)
-            os.symlink(link, output)
-
-    if args.dryrun:
+    if not args.live:
         log("DRYRN", "This was a dry run.")
 
 
